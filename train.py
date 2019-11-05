76989630a19a7ee98b210fd556dd0ac86857c151
= <=>
2019-11-05 06:11:35 +0000

    Add pratice solutions to train.py file

---
+++ train.py
@@ -0,0 +1,86 @@
+# Pratice 1
+import pandas as pd
+
+filename = 'chicago.csv'
+
+# load data file into a dataframe
+df = pd.read_csv(filename)
+
+# convert the Start Time column to datetime
+df['Start Time'] = pd.to_datetime(df['Start Time'], errors='coerce')
+
+# extract hour from the Start Time column to create an hour column
+df['hour'] = df['Start Time'].dt.hour
+
+# find the most common hour (from 0 to 23)
+popular_hour = df['hour'].mode()[0, 23]
+
+print('Most Frequent Start Hour:', popular_hour)
+
+# Pratice 2
+
+filename = 'chicago.csv'
+
+# load data file into a dataframe
+df = pd.read_csv(filename)
+
+# print value counts for each user type
+user_types = df['User Type'].value_counts()
+
+print(user_types)
+
+
+# Pratice 3
+
+import pandas as pd
+
+CITY_DATA = {'chicago': 'chicago.csv',
+             'new york city': 'new_york_city.csv',
+             'washington': 'washington.csv'}
+
+MONTHS = ['january', 'february', 'march', 'april', 'may', 'june']
+
+DAYS = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+
+
+def load_data(city, month, day):
+    """
+    Loads data for the specified city and filters by month and day if applicable.
+
+    Args:
+        (str) city - name of the city to analyze
+        (str) month - name of the month to filter by, or "all" to apply no month filter
+        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+    Returns:
+        df - pandas DataFrame containing city data filtered by month and day
+    """
+
+    # load data file into a dataframe
+    df = pd.read_csv(CITY_DATA[city])
+
+    # convert the Start Time column to datetime
+    df['Start Time'] = pd.to_datetime(df['Start Time'], errors='coerce')
+
+    # extract month and day of week from Start Time to create new columns
+    df['month'] = df['Start Time'].dt.month
+    df['day_of_week'] = df['Start Time'].dt.dayofweek
+
+    # filter by month if applicable
+    if month != 'all':
+        # use the index of the months list to get the corresponding int
+        months = ['january', 'february', 'march', 'april', 'may', 'june']
+        month = MONTHS.index(month) + 1
+
+        # filter by month to create the new dataframe
+        df = df[df.month == month]
+
+    # filter by day of week if applicable
+    if day != 'all':
+        # filter by day of week to create the new dataframe
+        df = df[df.day_of_week == DAYS.index(day)]
+
+    return df
+
+
+df = load_data('chicago', 'march', 'friday')
+
